d68af4158513eaa45d9824121fe08aefab19cbcc
patrick <patrickkuo@me.com>
2023-01-18 14:33:03 +0000

    * Added wrapped object to the indexes * get_past_object query the indexstore for wrapped object info, instead of returning object not found. * index_tx uses the new object to owner index instead of last modified at to work out the last owner information.

--- crates/sui-json-rpc-types/src/lib.rs
+++ crates/sui-json-rpc-types/src/lib.rs
@@ -1109,6 +1109,10 @@ pub enum SuiPastObjectRead<T: SuiData> {
         asked_version: SequenceNumber,
         latest_version: SequenceNumber,
     },
+    Wrapped {
+        object_id: ObjectID,
+        wrapped_version: SequenceNumber,
+    },
 }
 
 impl<T: SuiData> SuiPastObjectRead<T> {
@@ -1135,6 +1139,13 @@ impl<T: SuiData> SuiPastObjectRead<T> {
                 object_id: *object_id,
                 asked_version: *asked_version,
                 latest_version: *latest_version,
+            }),
+            SuiPastObjectRead::Wrapped {
+                object_id,
+                wrapped_version,
+            } => Err(SuiError::ObjectWrapped {
+                object_id: *object_id,
+                version: *wrapped_version,
             }),
         }
     }
@@ -1163,6 +1174,13 @@ impl<T: SuiData> SuiPastObjectRead<T> {
                 asked_version,
                 latest_version,
             }),
+            SuiPastObjectRead::Wrapped {
+                object_id,
+                wrapped_version,
+            } => Err(SuiError::ObjectWrapped {
+                object_id,
+                version: wrapped_version,
+            }),
         }
     }
 }
@@ -1190,6 +1208,13 @@ impl<T: SuiData> TryFrom<PastObjectRead> for SuiPastObjectRead<T> {
                 object_id,
                 asked_version,
                 latest_version,
+            }),
+            PastObjectRead::Wrapped {
+                object_id,
+                wrapped_version,
+            } => Ok(SuiPastObjectRead::Wrapped {
+                object_id,
+                wrapped_version,
             }),
         }
     }
