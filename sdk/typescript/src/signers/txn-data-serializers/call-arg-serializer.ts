28d5d3752da2baf6e6ba250f1d1997f8e4d5ccd6
siomari <maria@mystenlabs.com>
2023-01-26 17:25:13 +0200

    add uid in suijson and ts sdk

--- sdk/typescript/src/signers/txn-data-serializers/call-arg-serializer.ts
+++ sdk/typescript/src/signers/txn-data-serializers/call-arg-serializer.ts
@@ -17,6 +17,7 @@ import {
   SuiMoveNormalizedType,
   SuiObjectRef,
   SUI_FRAMEWORK_ADDRESS,
+  UID_STRUCT_NAME,
 } from '../../types';
 import { bcsForVersion, CallArg, isPureArg, MoveCallTx, ObjectArg, PureArg } from '../../types/sui-bcs';
 import { MoveCallTransaction } from './txn-data-serializer';
@@ -33,6 +34,12 @@ const RESOLVED_SUI_ID = {
   address: SUI_FRAMEWORK_ADDRESS,
   module: OBJECT_MODULE_NAME,
   name: ID_STRUCT_NAME,
+};
+
+const RESOLVED_SUI_UID = {
+  address: SUI_FRAMEWORK_ADDRESS,
+  module: OBJECT_MODULE_NAME,
+  name: UID_STRUCT_NAME,
 };
 
 const RESOLVED_ASCII_STR = {
@@ -307,6 +314,8 @@ export class CallArgSerializer {
       } else if (isSameStruct(normalizedType.Struct, RESOLVED_UTF8_STR)) {
         return 'utf8string';
       } else if (isSameStruct(normalizedType.Struct, RESOLVED_SUI_ID)) {
+        return 'address';
+      } else if (isSameStruct(normalizedType.Struct, RESOLVED_SUI_UID)) {
         return 'address';
       }
     }
