8a7041271506d621571e532393e8a2032153a239
Arun Koshy <97870774+arun-koshy@users.noreply.github.com>
2023-02-03 00:42:01 -0800

    Add multi-proxy support
    Enable stress client to accept a list of proxies and create a separate workload for each that sums up to the total tps & breakdown provided

--- crates/sui-benchmark/src/bin/stress.rs
+++ crates/sui-benchmark/src/bin/stress.rs
@@ -67,10 +67,12 @@ async fn main() -> Result<()> {
     let barrier = Arc::new(Barrier::new(2));
     let cloned_barrier = barrier.clone();
     let env = if opts.local { Env::Local } else { Env::Remote };
-    let benchmark_setup = env.setup(cloned_barrier, &registry, &opts).await?;
+    let proxy_gas_and_coins = env.setup(cloned_barrier, &registry, &opts).await?;
     let system_state_observer = {
+        // Only need to get system state from one proxy as it is shared for the
+        // whole network.
         let mut system_state_observer =
-            SystemStateObserver::new(benchmark_setup.validator_proxy.clone());
+            SystemStateObserver::new(proxy_gas_and_coins[0].proxy.clone());
         system_state_observer.reference_gas_price.changed().await?;
         eprintln!(
             "Found reference gas price from system state object = {:?}",
@@ -97,15 +99,9 @@ async fn main() -> Result<()> {
             } else {
                 WorkloadConfiguration::Combined
             };
-            let workloads = workload_configuration
-                .configure(
-                    benchmark_setup.primary_gas,
-                    benchmark_setup.pay_coin,
-                    benchmark_setup.pay_coin_type_tag,
-                    benchmark_setup.validator_proxy.clone(),
-                    &opts,
-                    system_state_observer.clone(),
-                )
+
+            let proxy_workloads = workload_configuration
+                .configure(proxy_gas_and_coins, &opts, system_state_observer.clone())
                 .await?;
             let interval = opts.run_duration;
             // We only show continuous progress in stderr
@@ -116,8 +112,7 @@ async fn main() -> Result<()> {
             let driver = BenchDriver::new(opts.stat_collection_interval, stress_stat_collection);
             driver
                 .run(
-                    workloads,
-                    benchmark_setup.validator_proxy.clone(),
+                    proxy_workloads,
                     system_state_observer,
                     &registry_clone,
                     show_progress,
