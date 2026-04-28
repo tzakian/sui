0217b16cd5ba42b93630581f649062ef3f2747c1
Tim Zakian <timzakian@gmail.com>
2026-04-28 08:19:47 -0700

    [move-core-types] Convert visitors to use compressed type layouts

--- external-crates/move/crates/move-core-types/src/compressed/runtime/layout.rs
+++ external-crates/move/crates/move-core-types/src/compressed/runtime/layout.rs
@@ -304,7 +304,11 @@ impl MoveStructLayout {
 
 impl fmt::Display for MoveStructLayout {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "struct {}", self.0)
+        if f.alternate() {
+            write!(f, "struct {:#}", self.0)
+        } else {
+            write!(f, "struct {}", self.0)
+        }
     }
 }
 
